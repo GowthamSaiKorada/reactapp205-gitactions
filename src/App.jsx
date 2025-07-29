4942af55d4d8d1d21a21c267c23a024b44523b5e
Gowtham <2300032467@kluniversity.in>
2025-07-29 12:37:34 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <h1>Welcome to My React Project - S205</h1>
       <p>CI/CD and Cloud DevOps</p>
       <p>This is a simple homepage deployed on GitHub ACtions using Vite.</p>
+      <p>Done with Git Pages and Git Actions</p>
     </div>
   );
 }
